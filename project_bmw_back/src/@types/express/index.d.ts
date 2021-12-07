956389ba8fb6ef5f2df9ad14f07e520ed4e3f52b
jae hyun <1994dbwogus@gmail.com>
2021-12-08 03:08:41 +0900

    fix: error TS2339 에러 수정
    1. src\@type\express 수정
      - Express 모듈의 interface Request에 속성 추가
    2. tsconfig.json에 규칙 추가

--- project_bmw_back/src/@types/express/index.d.ts
+++ project_bmw_back/src/@types/express/index.d.ts
@@ -1,22 +1,26 @@
 import { IUser } from '@entities/User';
 import { IClientData } from '@shared/JwtService';
-
+import { Dto } from '../dto/dto.interface';
 
 declare module 'express' {
-    export interface Request  {
-        body: {
-            user: IUser
-            email: string;
-            password: string;
-        };
-    }
+  export interface Request {
+    body: {
+      user: IUser;
+      email: string;
+      password: string;
+    };
+  }
 }
 
-
 declare global {
-    namespace Express {
-        export interface Response {
-            sessionUser: IClientData;
-        }
+  namespace Express {
+    export interface Response {
+      sessionUser: IClientData;
     }
+
+    // Express.Requset에 프로퍼티 추가
+    interface Request {
+      dto: Dto;
+    }
+  }
 }
