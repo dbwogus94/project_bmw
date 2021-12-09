b3e2d92a8d48fbce28403fd010011a6566bdbf47
jae hyun <1994dbwogus@gmail.com>
2021-12-10 05:22:26 +0900

    refactor: auth 미들웨어 코드 개선

--- project_bmw_back/src/middleware/auth.ts
+++ project_bmw_back/src/middleware/auth.ts
@@ -22,30 +22,25 @@ export const isAuth = async (req: Request, res: Response, next: NextFunction) =>
     return res.status(UNAUTHORIZED).json(errMessage);
   }
 
-  // TODO: 코드 개선 예정
-  // 2. jwt 디코딩
-  jwtService
-    .decodeJwt(accessToken, jwt.access.secret)
-    .then((payload: IJwtPayload) => {
-      const { id, username } = payload;
+  try {
+    // 2. jwt 디코딩
+    const payload: any = await jwtService.decodeToken(accessToken, jwt.access.secret);
 
-      // 3. 유효한 토큰이면? 블랙 리스트 토큰 조회
-      getClient()
-        .get(username)
-        .then((blacklistToken: string | null) => {
-          // 4. 블랙 리스트이면? 401 응답
-          if (blacklistToken && blacklistToken === accessToken) {
-            res.status(UNAUTHORIZED).json(errMessage);
-          }
-          // 4. 블랙리스트에 없거나 아니라면? req에 디코딩 데이터 담고 통과
-          req.id = id!;
-          req.username = username!;
-          next();
-        });
-    })
-    .catch(error => {
-      // 2. jwt 디코딩 실패 -> refresh 호출
-      const { username } = req.query;
-      return res.redirect(MOVED_TEMPORARILY, `/api/auth/refresh?username=${username}`);
-    });
+    // 3. 유효한 토큰이면? 블랙 리스트 토큰 조회
+    const { id, username } = payload;
+    const blacklistToken = await getClient().get(username);
+
+    // 4. 블랙 리스트이면? 401 응답
+    if (blacklistToken && blacklistToken === accessToken) {
+      res.status(UNAUTHORIZED).json(errMessage);
+    }
+    // 4. 블랙리스트에 없거나 아니라면? req에 디코딩 데이터 담고 통과
+    req.id = id!;
+    req.username = username!;
+    next();
+  } catch (error) {
+    // 2. jwt 디코딩 실패 -> refresh 호출
+    const { username } = req.query;
+    return res.redirect(MOVED_TEMPORARILY, `/api/auth/refresh?username=${username}`);
+  }
 };
