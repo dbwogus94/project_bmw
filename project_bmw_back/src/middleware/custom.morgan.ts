0c0e320c3baab0c44815f448c0818108379ca420
jae hyun <1994dbwogus@gmail.com>
2021-12-12 07:25:33 +0900

    feat: 커스텀 morgan 미들웨어 생성
    - http.logger에서 생성한 로거를 사용하는 몰간 미들웨어

---
+++ project_bmw_back/src/middleware/custom.morgan.ts
@@ -0,0 +1,30 @@
+import morgan, { StreamOptions } from 'morgan';
+import { createHttpLogger, getHttpLogger } from '@shared/http.logger';
+import { config } from '@config';
+
+createHttpLogger('api-server');
+const httpLogger = getHttpLogger();
+const { environment } = config;
+
+// Override the stream method by telling
+// Morgan은 console.log 대신 윈스턴 커스텀 로거를 사용한다.
+const stream: StreamOptions = {
+  write: message => httpLogger.http(message),
+};
+
+// Morgan http 로그 스킵 설정.
+const skip = () => {
+  return environment !== 'development';
+};
+
+// Build morgan middleware
+const customMorgan = () =>
+  morgan(
+    environment === 'development' ? 'dev' : 'combined', // 출력 포멧 설정
+    {
+      // skip, // 로그 스킵 설정
+      stream, // 몰간이 사용할 로거 설정
+    },
+  );
+
+export default customMorgan;
