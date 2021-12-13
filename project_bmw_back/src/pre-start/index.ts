75e1aa87d2786a25fc751cde3db76ece2a6d5426
jae hyun <1994dbwogus@gmail.com>
2021-12-13 21:06:23 +0900

    refactor: env 파일 읽어오는 위치 변경

--- project_bmw_back/src/pre-start/index.ts
+++ project_bmw_back/src/pre-start/index.ts
@@ -7,23 +7,22 @@ import path from 'path';
 import dotenv from 'dotenv';
 import commandLineArgs from 'command-line-args';
 
-
-
 (() => {
-    // Setup command line options
-    const options = commandLineArgs([
-        {
-            name: 'env',
-            alias: 'e',
-            defaultValue: 'development',
-            type: String,
-        },
-    ]);
-    // Set the env file
-    const result2 = dotenv.config({
-        path: path.join(__dirname, `env/${options.env}.env`),
-    });
-    if (result2.error) {
-        throw result2.error;
-    }
+  // Setup command line options
+  const options = commandLineArgs([
+    {
+      name: 'env',
+      alias: 'e',
+      defaultValue: 'development',
+      type: String,
+    },
+  ]);
+  // Set the env file
+  console.log(__dirname);
+  const result2 = dotenv.config({
+    path: path.join(process.cwd(), `env/${options.env}.env`),
+  });
+  if (result2.error) {
+    throw result2.error;
+  }
 })();
