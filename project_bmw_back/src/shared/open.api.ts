5ca007c0881220d13a7ce8cc0182fed8905636d5
jae hyun <1994dbwogus@gmail.com>
2021-12-24 01:56:12 +0900

    feat: Open API 호출용 클래스 생성
    - 공공데이터 Open API 호출과 응답 결과를 JSON으로 파싱한 결과를 리턴한다.

---
+++ project_bmw_back/src/shared/open.api.ts
@@ -0,0 +1,42 @@
+import { XMLParser } from 'fast-xml-parser';
+import axios from 'axios';
+
+export class OpenApi {
+  private parser: XMLParser;
+
+  constructor(parser: XMLParser) {
+    this.parser = parser;
+  }
+
+  /**
+   * xml to json parser
+   * @param body - xml형식을 가진 문자열
+   * @returns
+   */
+  private parseJson(body: string) {
+    return this.parser.parse(body);
+  }
+
+  /**
+   * open api호출하고 결과를 json으로 반환한다.
+   * - open api에 따라 에러 응답이 다르다.
+   * - 때문에 직접적인 에러 헨들링은 사용하는 곳에서 처리한다.
+   * - 단, 200 응답이 오지 않은 경우에는 에러를 던진다.
+   * @param url
+   * @returns
+   */
+  async callApi(url: string) {
+    const res = await axios({
+      method: 'get',
+      url,
+    });
+
+    // open API는 REST하지 않음. 요청이 잘못되어도 200으로 응답한다.
+    if (res.status !== 200) {
+      throw new Error('[OpenApi] API 서버에 장애가 있습니다.');
+    }
+
+    // xml to json
+    return this.parseJson(res.data);
+  }
+}
