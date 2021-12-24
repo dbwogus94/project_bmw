7121d2180989e1b008ad7e6dba7573891fa0698d
jae hyun <1994dbwogus@gmail.com>
2021-12-24 23:24:03 +0900

    refactor: bus.search.dto에 위생화 옵션 추가

--- project_bmw_back/src/bus/dto/request/bus.search.dto.ts
+++ project_bmw_back/src/bus/dto/request/bus.search.dto.ts
@@ -1,8 +1,10 @@
 import { Dto } from '@user/dto/dto.interface';
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class BusSearchDto implements Dto {
   @IsNotEmpty()
   @IsString()
+  @Transform(params => params.value.trim()) // 위생화
   public routeName!: string;
 }
