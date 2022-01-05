f6b0843e6c850d06cd2fa7adfbc9013b2cf23d4c
jae hyun <1994dbwogus@gmail.com>
2022-01-06 05:23:17 +0900

    feat(back): BookMark Entity 수정
    - BookMark 엔티티 api의 데이터 저장을 위해 컬럼 추가
    - 저장용 Column에는 select: false 옵션을 부여하였음
    - typeOrm에서는 select: false가 부여된 컬럼은 단순 조회로 조회가 불가능하다.
    - 엔티티 변경에 따라 CreateBookMarkDto도 수정

--- project_bmw_back/src/book-mark/dto/request/create-book-mark.dto.ts
+++ project_bmw_back/src/book-mark/dto/request/create-book-mark.dto.ts
@@ -1,30 +1,28 @@
 import { Dto } from '@user/dto/dto.interface';
-import { IsNotEmpty, IsNumber, IsString, Matches } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Matches } from 'class-validator';
 
 export class CreateBookMarkDto implements Dto {
   @IsNotEmpty()
+  @Type(() => Number)
   @IsNumber()
-  public bmGroupId!: number;
+  public bmGroupId!: number; // 그룹 id
 
+  /* BookMark entity*/
   @IsNotEmpty()
+  @Type(() => Number)
   @IsNumber()
   public routeId!: number; // 노선 Id
 
   @IsNotEmpty()
-  @IsString()
-  public routeName!: string; // 노선 이름
-
-  @IsNotEmpty()
+  @Type(() => Number)
   @IsNumber()
-  public stationSeq!: number; // 노선 경유 정류소 순번
+  public stationSeq!: number; // 경유 정류소 순번
 
   @IsNotEmpty()
+  @Type(() => Number)
   @IsNumber()
   public stationId!: number; // 정류소 Id
-
-  @IsNotEmpty()
-  @IsString()
-  public stationName!: string; // 정류소 이름
 
   @IsNotEmpty()
   @Matches(/B|M/i)
@@ -32,13 +30,80 @@ export class CreateBookMarkDto implements Dto {
 
   @IsNotEmpty()
   @IsString()
-  public direction!: string; // 노선 진행 방향
+  @Transform(params => params.value.trim())
+  public routeName!: string; // 노선 이름
 
   @IsNotEmpty()
   @IsString()
-  public routeTypeName!: string; //노선 유형
+  @Transform(params => params.value.trim())
+  public stationName!: string; // 정류소 이름
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(params => params.value.trim())
+  public direction!: string; // 노선 진행방향
 
   @IsNotEmpty()
   @Matches(/seoul|gyeonggi/i)
   public type!: 'seoul' | 'gyeonggi'; // api type
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(params => params.value.trim())
+  public startStationName!: string; // 기점 정류소
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(params => params.value.trim())
+  public endStationName!: string; // 종점 정류소
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  public routeTypeCd!: number; // 노선 종류코드
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(params => params.value.trim())
+  public routeTypeName!: string; // 노선 종류이름
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(params => params.value.trim())
+  public regionName!: string; // 노선 운행지역명
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  public districtCd!: number; // 관할지역코드
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(params => params.value.trim())
+  public districtName!: string; // 관할지역명
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  public minTerm!: number; // 최소배차
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  public maxTerm!: number; // 최대배차
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  public companyId!: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(params => params.value.trim())
+  public companyName!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(params => params.value.trim())
+  public companyTel!: string;
 }
--- project_bmw_back/src/book-mark/entities/BookMark.entity.ts
+++ project_bmw_back/src/book-mark/entities/BookMark.entity.ts
@@ -3,6 +3,9 @@ import { BmGroupBookMark } from '@bmGroupBookMark/entities/BmGroupBookMark.entit
 
 export interface IBookMark {
   bookMarkId: number;
+  checkColumn: string;
+
+  /* client 사용 */
   routeId: number;
   stationSeq: number;
   stationId: number;
@@ -10,8 +13,23 @@ export interface IBookMark {
   routeName: string;
   stationName: string;
   direction: string;
-  routeTypeName: string;
   type: 'seoul' | 'gyeonggi';
+
+  /* 선택 */
+  startStationName: string;
+  endStationName: string;
+  routeTypeCd: number;
+  routeTypeName: string;
+  regionName: string;
+  districtCd: number;
+  districtName: string;
+  minTerm: number;
+  maxTerm: number;
+  companyId: number;
+  companyName: string;
+  companyTel: string;
+
+  /* */
   createdAt: Date;
   updatedAt: Date;
   bmGroupBookMarks: BmGroupBookMark[];
@@ -22,10 +40,18 @@ export class BookMark implements IBookMark {
   @PrimaryGeneratedColumn({ comment: 'bookMark PK' })
   bookMarkId!: number;
 
-  @Column('int', { comment: '노선 ID' })
+  @Column('varchar', {
+    length: 100,
+    unique: true,
+    nullable: false,
+    comment: '중복된 데이터 체크용 컬럼:  String(routeId) + String(stationSeq) + String(stationId)',
+  })
+  checkColumn!: string;
+
+  @Column('int', { comment: '노선ID' })
   routeId!: number;
 
-  @Column('int', { comment: '경유 정류소(역) 순서' })
+  @Column('int', { comment: '경유정류소(역) 순서' })
   stationSeq!: number;
 
   @Column('int', { comment: '정유소(역) ID' })
@@ -34,21 +60,59 @@ export class BookMark implements IBookMark {
   @Column('varchar', { length: 2, comment: '버스(B), 지하철(M) 구분용 라벨' })
   label!: 'B' | 'M';
 
-  @Column('varchar', { length: 60, comment: '노선 이름' })
+  @Column('varchar', { length: 60, comment: '노선이름' })
   routeName!: string;
 
   @Column('varchar', { length: 300, comment: '정류소(역) 이름' })
   stationName!: string;
 
-  @Column('varchar', { length: 200, comment: '노선 진행 방향' })
+  @Column('varchar', { length: 200, comment: '노선진행방향' })
   direction!: string;
-
-  @Column('varchar', { length: 60, comment: '노선 종류(타입)' })
-  routeTypeName!: string;
 
   @Column('varchar', { length: 20, comment: 'Open API 종류' })
   type!: 'seoul' | 'gyeonggi';
 
+  /* 데이터 저장용  
+  - select: false가 부여된 컬럼은 typeOrm 사용시 일반적인 방법으로는 조회하지 못한다.
+  - createQueryBuilder()에서 select()나 addSelect()을 사용하여 명시적으로 선언해야 사용이 가능하다.
+  */
+  @Column('varchar', { length: 300, comment: '기점정류소명', select: false })
+  startStationName!: string;
+
+  @Column('varchar', { length: 300, comment: '종점정류소명', select: false })
+  endStationName!: string;
+
+  @Column('int', { comment: '노선종류코드' })
+  routeTypeCd!: number;
+
+  @Column('varchar', { length: 60, comment: '노선종류이름', select: false })
+  routeTypeName!: string;
+
+  @Column('varchar', { length: 60, comment: '노선운행지역명', select: false })
+  regionName!: string;
+
+  @Column('int', { comment: '관할지역코드(1: 서울, 2: 경기, 3: 인천)', select: false })
+  districtCd!: number;
+
+  @Column('varchar', { length: 6, comment: '관할지역명', select: false })
+  districtName!: string;
+
+  @Column('int', { comment: '최소배차시간', select: false })
+  minTerm!: number;
+
+  @Column('int', { comment: '최대배차시간', select: false })
+  maxTerm!: number;
+
+  @Column('int', { nullable: true, comment: '운수업체ID', select: false })
+  companyId!: number;
+
+  @Column('varchar', { length: 200, comment: '운수업체명', select: false })
+  companyName!: string;
+
+  @Column('varchar', { length: 50, comment: '운수업체 전화번호', select: false })
+  companyTel!: string;
+
+  /* 시스템 사용 */
   @CreateDateColumn({ comment: '생성일' })
   createdAt!: Date;
 
