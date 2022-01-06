350bbaa050ce3bce8f54ef0c4de6d635c2434529
jae hyun <1994dbwogus@gmail.com>
2022-01-06 23:22:56 +0900

    fix(back): GetBmGroupDto에 누락된 속성 추가

--- project_bmw_back/src/bm-group/dto/request/get-bm-group.dto.ts
+++ project_bmw_back/src/bm-group/dto/request/get-bm-group.dto.ts
@@ -1,10 +1,15 @@
 import { Dto } from '@user/dto/dto.interface';
 import { Type } from 'class-transformer';
-import { IsNotEmpty, IsNumber, IsNumberString } from 'class-validator';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class GetBmGroupDto implements Dto {
   @IsNotEmpty()
   @Type(() => Number)
   @IsNumber()
   public bmGroupId!: number;
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  public bookMarkId!: number;
 }
